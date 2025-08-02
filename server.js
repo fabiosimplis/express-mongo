dc28d972a4ba22031342931e1c85bfb9dd75a152
fabiosimplis <fabio.simplis@gmail.com>
2025-08-02 16:48:15 -0300

    Criando rotas

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 import http from "http";
+
 const PORT = 3000;
+
+const rotas = {
+  "/": "Curso de Express API",
+  "/livros": "Rota Livros",
+  "/autores": "Rota autores"
+}
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-type": "text/plain"})
-  res.end("Curso de Node.js");
+  res.end(rotas[req.url]);
 });
 
 server.listen(PORT, () => {
