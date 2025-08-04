6d31bc4ac07b97b87616e346f579d4ba9d94f5f5
fabiosimplis <fabio.simplis@gmail.com>
2025-08-03 23:14:37 -0300

    Busca por parametro query

--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -3,8 +3,11 @@ import LivroController from "../controllers/livroController.js";
 
 const routes = express.Router();
 
+// Express organiza por precedencia de rotas, ou seja, as rotas são chamadas em sequencia.
+// assim devemos adicionar conforme a complexidade, da maior para menor
 routes.get("/livros", LivroController.listarLivros);
-routes.get("/livros/:id", LivroController.listarLivroPorId);
+routes.get("/livros/busca", LivroController.listarLivrosPorEditora);
+routes.get("/livros/:id", LivroController.deletarLivroPorId);
 routes.post("/livros", LivroController.cadastrarLivro);
 routes.put("/livros/:id", LivroController.atualizarLivro);
 routes.delete("/livros/:id", LivroController.deletarLivroPorId);
